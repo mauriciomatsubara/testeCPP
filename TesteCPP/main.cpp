618f5dd1c1ea627f2aebe8ada38eacf7875d9a97
Mauricio Matsubara <mauricio@matsubara.co>
2017-02-09 16:25:23 -0200

    Initial Commit

---
+++ TesteCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TesteCPP
+//
+//  Created by Mauricio Matsubara on 09/02/17.
+//  Copyright © 2017 Mauricio Matsubara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
